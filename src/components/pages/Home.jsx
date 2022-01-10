1a39c568716a8536d30302a3734bfea53bcb7eb8
anderson <anderson.d.cardoso92@gmail.com>
2022-01-10 10:41:02 -0600

    add cards and card item components

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroSection from '../HeroSection';
+import Cards from '../Cards';
 import Footer from '../Footer';
 
 const Home = () => {
     return (
         <>
             <HeroSection />
+            <Cards />
             <Footer />
         </>
     )
